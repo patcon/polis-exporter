142e515372eb90d5473f3975296654241e8362fb
Patrick Connolly <patrick.c.connolly@gmail.com>
2024-04-11 01:27:39 -0500

    Added makeSummary.json to generate summary.csv.

---
+++ makeSummary.js
@@ -0,0 +1,23 @@
+import fs from 'node:fs/promises'
+
+const args = process.argv.slice(2);
+const convoId = args[0]
+const CONVO_ID = convoId || "6bkf4ujff9"
+
+const convo = JSON.parse(await fs.readFile(`data/${CONVO_ID}--conversations.json`, "binary"))
+const pca2 = JSON.parse(await fs.readFile(`data/${CONVO_ID}--math-pca2.json`, "binary"))
+const convoStats = JSON.parse(await fs.readFile(`data/${CONVO_ID}--conversationStats.json`, "binary"))
+
+const summary = {
+  topic: convo.topic,
+  url: `https://pol.is/${CONVO_ID}`,
+  views: convo.participant_count,
+  voters: pca2["n"],
+  "voters-in-conv": pca2["n"],
+  commenters: convoStats.firstCommentTimes.length,
+  comments: pca2["n-cmts"],
+  groups: pca2["group-clusters"].length,
+  "conversation-description": convo.description,
+}
+
+console.log(summary)
